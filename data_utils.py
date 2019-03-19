a5b38ead34e9c34ae6f2fbed498c08f43c997dae
Hoang Loc <hoangloc0402@gmail.com>
2019-03-19 11:06:27 +0700

    Finish price norm

--- data_utils.py
+++ data_utils.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[5]:
 
 
 import re
 import glob
 import string
 import ast
-import unicode_utils 
 import unicodedata
 from pyvi import ViTokenizer, ViPosTagger
 
@@ -102,28 +101,29 @@ def clean_text(text):
     return re.sub(r'[ ]+', ' ', text), numbers
 
 
-# In[ ]:
+# In[6]:
 
 
 def remove_vietnamese_accent(s):
 #     s = s.decode('utf-8')
     s = re.sub(u'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
     s = re.sub(u'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
-    s = re.sub(u'èéẹẻẽêềếệểễ', 'e', s)
-    s = re.sub(u'ÈÉẸẺẼÊỀẾỆỂỄ', 'E', s)
-    s = re.sub(u'òóọỏõôồốộổỗơờớợởỡ', 'o', s)
-    s = re.sub(u'ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ', 'O', s)
-    s = re.sub(u'ìíịỉĩ', 'i', s)
-    s = re.sub(u'ÌÍỊỈĨ', 'I', s)
-    s = re.sub(u'ùúụủũưừứựửữ', 'u', s)
-    s = re.sub(u'ƯỪỨỰỬỮÙÚỤỦŨ', 'U', s)
-    s = re.sub(u'ỳýỵỷỹ', 'y', s)
-    s = re.sub(u'ỲÝỴỶỸ', 'Y', s)
-    s = re.sub(u'Đ', 'D', s)
-    s = re.sub(u'đ', 'd', s)
+    s = re.sub(u'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(u'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(u'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(u'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(u'[ìíịỉĩ]', 'i', s)
+    s = re.sub(u'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(u'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(u'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(u'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(u'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(u'[Đ]', 'D', s)
+    s = re.sub(u'[đ]', 'd', s)
 #     return s.encode('utf-8')
     s = s.lower()
     return s
+
 
 
 # In[2]:
